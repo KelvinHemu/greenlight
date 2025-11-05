e361490b14b1dde30faca410dc6af4ec7c13b142
Kelvin Hemu <kelvinhemu@gmail.com>
2025-11-05 17:55:11 +0300

    Implement database connection pooling and add movie model with insert functionality

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,24 +10,28 @@ import (
 	"os"
 	"time"
 
-	_ "github.com/lib/pq"
+	"greenlight.chriss875.net/internal/data"
 
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
 
-
 type config struct {
 	port int
 	env  string
-	db 	 struct{
-		dsn string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
 	}
 }
 
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -35,7 +39,12 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|production)")
+
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
@@ -52,8 +61,8 @@ func main() {
 	app := application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
-
 
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
@@ -69,13 +78,25 @@ func main() {
 	logger.Fatal(err)
 }
 
-
 func openDB(cfg config) (*sql.DB, error) {
 	// sql.Open() to create an empty connection pool, using the DSN from the config
 	db, err := sql.Open("postgres", cfg.db.dsn)
 	if err != nil {
+
+		return nil, err
+
+	}
+
+	// Database connection pool configuration
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
 		return nil, err
 	}
+
+	db.SetConnMaxIdleTime(duration)
 
 	// a context with a 5-second timeout deadline.
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
